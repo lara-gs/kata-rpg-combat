30dcdd8746b643574c0447fe04e0cc08e7c18a66
lara-gs <lara_989@hotmail.com>
2024-10-28 19:29:30 +0100

    added iteration 3 and its tests

--- src/main/java/org/factoriaf5/kata_rpg/Character.java
+++ src/main/java/org/factoriaf5/kata_rpg/Character.java
@@ -4,25 +4,41 @@ public class Character {
     private int health = 1000;
     private int level = 1;
     private boolean alive = true;
+    private double attackRange;
 
-    public void dealDamage(Character target, int damage) {
-        if (this != target && target.isAlive()) {
+    public Character() {
+    this(true); 
+}
+
+    public Character(boolean isMelee) {
+        this.attackRange = calculateAttackRange(isMelee);
+
+    }
+
+    private double calculateAttackRange(boolean isMelee) {
+        if (isMelee) {
+            return 2.0;
+        } else {
+            return 20.0;
+        }
+    }
+
+    public void dealDamage(Character target, int damage, double distance) {
+        if (this != target && target.isAlive() && distance <= attackRange) {
             if (target.level >= this.level + 5) {
                 damage /= 2;
             } else if (this.level >= target.level + 5) {
-                damage *= 1.5; 
+                damage *= 1.5;
             }
-    
 
             target.health -= damage;
-    
+
             if (target.health <= 0) {
                 target.alive = false;
-                target.health = 0; 
+                target.health = 0;
             }
         }
     }
-    
 
     public void heal(Character target, int healingAmount) {
         if (this == target && target.isAlive()) {
@@ -42,4 +58,10 @@ public class Character {
         return alive;
     }
 
+    public double getAttackRange() {
+        return attackRange;
+    }
+    public int getLevel(){
+        return level;
+    }
 }
--- src/test/java/org/factoriaf5/kata_rpg/TestCharacter.java
+++ src/test/java/org/factoriaf5/kata_rpg/TestCharacter.java
@@ -13,18 +13,18 @@ public class TestCharacter {
 }
 @Test
 void testDealDamageReducesHealth() {
-    Character attacker = new Character(); 
-    Character target = new Character();    
-    attacker.dealDamage(target, 200);     
+    Character attacker = new Character(true); 
+    Character target = new Character(true);    
+    attacker.dealDamage(target, 200, 1.0);     
     assertThat(target.getHealth(), is(800)); 
 }
 
 
     @Test
     void testHealthMax() {
-        Character attacker = new Character();
-        Character target = new Character();
-        attacker.dealDamage(target, 200);
+        Character attacker = new Character(true);
+        Character target = new Character(true);
+        attacker.dealDamage(target, 200, 1.0);
         target.heal(target, 300);
         assertThat(target.getHealth(), is(1000));
 
@@ -34,7 +34,7 @@ void testDealDamageReducesHealth() {
     void testHealingDeadCharacter() {
         Character target = new Character();
         Character attacker = new Character();
-        attacker.dealDamage(target, 1000);
+        attacker.dealDamage(target, 1000, 1.0);
         target.heal(target, 200);
         assertThat(target.getHealth(), is(0));
         assertThat(target.isAlive(), is(false));
@@ -44,8 +44,8 @@ void testDealDamageReducesHealth() {
     void testCharacterDeadCannotTakeDamage(){
         Character attacker = new Character();
         Character target = new Character();
-        attacker.dealDamage(target, 1000);
-        attacker.dealDamage(target, 150);
+        attacker.dealDamage(target, 1000, 1.0);
+        attacker.dealDamage(target, 150, 1.0);
         assertThat(target.getHealth(), is(0));
         assertThat(target.isAlive(), is (false));
 
@@ -55,7 +55,7 @@ void testDealDamageReducesHealth() {
     void testDiesAfterCriticalHit() {
         Character attacker = new Character();
         Character target = new Character();
-        attacker.dealDamage(target, 1000);
+        attacker.dealDamage(target, 1000, 1.0);
         assertThat(target.getHealth(), is(0)); 
         assertThat(target.isAlive(), is (false));
     }
@@ -67,6 +67,39 @@ void testDealDamageReducesHealth() {
         assertThat(character.isAlive(), is (true));
 
     }
+    @Test
+    void testDealDamageOutofRange(){
+        Character attacker = new Character(true);
+        Character target = new Character(true);
+        attacker.dealDamage(target, 100, 4.0);
+        assertThat(target.getHealth(), is(1000));
+        assertThat(target.isAlive(),is (true));
+    }
+
+    @Test
+    void testCannotHealCharacterWhenIDead(){
+        Character target = new Character(true);
+        Character healer = new Character(true);
+        healer.dealDamage(target, 1000, 1.5);
+        healer.heal(target,100);
+        assertThat(target.getHealth(), is(0));
+        assertThat(target.isAlive(), is(false));
+    }
+    @Test
+    void testCannotHurtMySelf() {
+        Character character = new Character();
+        character.dealDamage(character, 200, 1.0);
+        assertThat(character.getHealth(), is(1000));
+    }
+
+    @Test
+    void testRangeCharacterCanDealDamageWwithinRange(){
+        Character attacker = new Character(false);
+        Character target = new Character(true);
+        attacker.dealDamage(target, 500, 12.0);
+        assertThat(target.getHealth(), is(500));
+    }
+
     }
 
 
