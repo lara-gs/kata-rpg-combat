bb72fe26e2fe2b538ad0a4e61bb9a26fdda13c1d
lara-gs <lara_989@hotmail.com>
2024-10-28 18:44:13 +0100

    method heal fixed and test added

--- src/test/java/org/factoriaf5/kata_rpg/TestCharacter.java
+++ src/test/java/org/factoriaf5/kata_rpg/TestCharacter.java
@@ -22,38 +22,42 @@ void testDealDamageReducesHealth() {
 
     @Test
     void testHealthMax() {
-        Character character = new Character();
-        character.dealDamage(200);
-        character.heal(300);
-        assertThat(character.getHealth(), is(1000));
+        Character attacker = new Character();
+        Character target = new Character();
+        attacker.dealDamage(target, 200);
+        target.heal(target, 300);
+        assertThat(target.getHealth(), is(1000));
 
     }
 
     @Test
     void testHealingDeadCharacter() {
-        Character character = new Character();
-        character.dealDamage(1000);
-        character.heal(200);
-        assertThat(character.getHealth(), is(0));
-        assertThat(character.isAlive(), is(false));
+        Character target = new Character();
+        Character attacker = new Character();
+        attacker.dealDamage(target, 1000);
+        target.heal(target, 200);
+        assertThat(target.getHealth(), is(0));
+        assertThat(target.isAlive(), is(false));
     }
 
     @Test
     void testCharacterDeadCannotTakeDamage(){
-        Character character = new Character();
-        character.dealDamage(1000);
-        character.dealDamage(150);
-        assertThat(character.getHealth(), is(0));
-        assertThat(character.isAlive(), is (false));
+        Character attacker = new Character();
+        Character target = new Character();
+        attacker.dealDamage(target, 1000);
+        attacker.dealDamage(target, 150);
+        assertThat(target.getHealth(), is(0));
+        assertThat(target.isAlive(), is (false));
 
     }
 
     @Test 
     void testDiesAfterCriticalHit() {
-        Character character = new Character();
-        character.dealDamage(1000);
-        assertThat(character.getHealth(), is(0)); 
-        assertThat(character.isAlive(), is (false));
+        Character attacker = new Character();
+        Character target = new Character();
+        attacker.dealDamage(target, 1000);
+        assertThat(target.getHealth(), is(0)); 
+        assertThat(target.isAlive(), is (false));
     }
 
     @Test
